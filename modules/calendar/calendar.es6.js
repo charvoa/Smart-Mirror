d7ad9a4f9b9a9c8aba5724cc47e5bceb6fd4e65d
Nicolas Charvoz <nicolas.charvoz@epitech.eu>
2016-07-20 16:43:23 +0200

    update

--- modules/calendar/calendar.es6.js
+++ modules/calendar/calendar.es6.js
@@ -41,10 +41,11 @@ class calendar extends Module{
 
     getDate() {
         var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+        var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
 
-var day = days[ now.getDay() ];
-var month = months[ now.getMonth() ];
+        var day = days[ now.getDay() ];
+        var month = months[ now.getMonth() ];
+}
 }
 
 module.exports = calendar;
